3b33a7182d7fdc0add0cc1a5ff3699e45911ec8d
kepha <kepha@DESKTOP-830OF6F>
2016-12-09 16:04:50 -0600

    Add project files.

---
+++ MediaViewer/Presenters/MenuPresenter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediaViewer.Views;
+
+namespace MediaViewer.Presenters
+{
+    public class MenuPresenter
+    {
+        private readonly ApplicationController _controller;
+        public MenuPresenter(ApplicationController controller)
+        {
+            _controller = controller;
+            _controller.DisplayInShell(new MenuView(this));
+        }
+        public void DisplayPictures()
+        {
+        }
+        public void ListenToMusic()
+        {
+        }
+        public void WatchVideo()
+        {
+        }
+    }
+}
